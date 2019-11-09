bfa861d93e679458780131eaa66b7f2cc503b097
chrismd2 <53451424+chrismd2@users.noreply.github.com>
2019-11-09 11:30:45 -0600

    Upload for Debug Variable
    Current running of the classes using a global debug variable

--- RainbowAttack.cpp
+++ RainbowAttack.cpp
@@ -1,1 +1,13 @@
+#include "Globals.cpp"
+#include <iostream>
+using namespace std;
+
+class RainbowAttack{
+public:
+    RainbowAttack();
+};
+
+RainbowAttack::RainbowAttack(){
+    if(DEBUG){cout << "RainbowAttack made\n";}
+}
 
